8f95288a473ab8aea4467e77367f38a8e442cee5
Samuel Akinwunmi <sammygfx@googlemail.com>
2022-02-22 00:56:26 +0000

    cleand up animal, and fixed errors with gen_server

--- part 2/animal.erl
+++ part 2/animal.erl
@@ -1,77 +1,21 @@
 -module(animal).
-
--export([start_link / 2, move/2, sleep/2, stop/1, move_coords/2]).
--export([init/3]).
+-compile(export_all).
 
 -record(animal, {name, xPosition, yPosition}).
 
 start_link(Name, {X, Y}) ->
-    Pid = spawn(?MODULE, init, [Name, X, Y]),
-    register(Name, Pid),
-    {ok, Pid}.
+    register(Name, spawn(?MODULE, init, [Name, X, Y])).
 
 init(Name, X, Y) ->
     io:format("~p: Started in position {~p,~p}~n", [Name, X, Y]),
-    ets:new(animalDb, [named_table, set, {keypos, #animal.name}]),
-    ets:insert(animalDb, #animal{name = Name, xPosition = X, yPosition = Y}),
-    loop(animalDb).
+    ets:insert(worldAnimalDb, #animal{name = Name, xPosition = X, yPosition = Y}).
 
-loop(AnimalDB) ->
-    receive
-        {moveCoords, From, Name, Coords} ->
-            {X, Y} = Coords,
-            ets:insert(AnimalDB, #animal{name = Name, xPosition = X, yPosition = Y}),
-            From ! {reply, positionIo(AnimalDB, Name)};
-        {move, From, Name, Direction} ->
-            case Direction of
-                up ->    ets:update_element(AnimalDB, Name, {4, lookup(AnimalDB, Name, y) + 1}),
-                         From ! {reply, positionIo(AnimalDB, Name)},
-                         loop(AnimalDB);
-                down ->  ets:update_element(AnimalDB, Name, {4, lookup(AnimalDB, Name, y) - 1}),
-                         From ! {reply, positionIo(AnimalDB, Name)},
-                         loop(AnimalDB);
-                left ->  ets:update_element(AnimalDB, Name, {3, lookup(AnimalDB, Name, x) - 1}),
-                         From ! {reply, positionIo(AnimalDB, Name)},
-                         loop(AnimalDB);
-                right -> ets:update_element(AnimalDB, Name, {3, lookup(AnimalDB, Name, x) + 1}),
-                         From ! {reply, positionIo(AnimalDB, Name)},
-                         loop(AnimalDB)
-            end;
-        {stop, Name} ->
-            terminate(Name)
-    end.
+move_coords(Name, Coords, AnimalDb) ->
+    {X, Y} = Coords,
+    ets:insert(worldAnimalDb, #animal{name = Name, xPosition = X, yPosition = Y}),
+    AnimalDb.
 
-move(Name, Move) ->
-    Name ! {move, self(), Name, Move},
-    receive
-        {reply, Reply} -> Reply
-    end.
-
-move_coords(Name, Coords) ->
-    Name ! {moveCoords, self(), Name, Coords},
-    receive
-        {reply, Reply} -> Reply
-    end.
-    
-sleep(_Name, Time) ->
-    receive
-        after Time -> ok
-    end.
-
-stop(Name) ->
-    io:format("Stopping ~p~n", [Name]),
-    Name ! {stop, self(), Name},
-    ok.
-
-terminate(Name) ->
-    exit(whereis(Name), kill).
-
-positionIo(Db, Key) ->
-    [{_,_,X,Y}] = ets:lookup(Db, Key),
-    io:format("~p: Moved to position {~p,~p}~n", [Key,X,Y]).
-
-lookup(Db, Key, XorY) ->
-    case XorY of
-        y -> [{_,_,_,Y}] = ets:lookup(Db, Key), Y;
-        x -> [{_,_,X,_}] = ets:lookup(Db, Key), X
-    end.
+% delete(Name, AnimalDb) ->
+%     io:format("Stopping ~p~n", [Name]),
+%     exit(whereis(Name), kill),
+%     AnimalDb.
--- part 2/world.erl
+++ part 2/world.erl
@@ -1,30 +1,30 @@
 -module(world).
+-compile(export_all).
 
--export([start_link/1, move/2, get/1, delete/1]).
--export([init/1, handle_call/3]).
-
--behaviour(gen_sever).
+-behaviour(gen_server).
 
 -record(gridSize, {widthX, widthY}).
--record(animal, {name, positionX, positionY}).
+-record(animal, {name, xPosition, yPosition}).
+
+-compile({no_auto_import,[get/1]}).
 
 start_link(Filename) ->
     {ok, [Config]} = file:consult(Filename),
     {GridSize, _Teleporters, Obstacles} = Config,
     grid_into_db(GridSize),
     obstacles_into_db(Obstacles),
+    %%% TODO -> Change ETS table to DETS or use mnesia
+    ets:new(worldAnimalDb, [named_table, set, {keypos, #animal.name}, public]),
     gen_server:start_link({local, ?MODULE},?MODULE,[],[]).
 
 init(_) ->
-    %%% Should I be initialising the turtle here? %%%
-    %%% Seeems strange not to give user interface %%%
+    %%% Need to initialise at least on animal to get started
     {ok, animal:start_link(turtle, {4, 3})}.
 
 grid_into_db(Grid) ->
     {WidthX, WidthY} = Grid,
     ets:new(gridDb, [named_table, set]),
-    ets:insert(gridDb, 
-        #gridSize{widthX = WidthX, widthY = WidthY}).
+    ets:insert(gridDb, #gridSize{widthX = WidthX, widthY = WidthY}).
 
 obstacles_into_db(Obstacles) ->
     [Palm, Rock, Water] = Obstacles,
@@ -35,19 +35,62 @@ obstacles_into_db(Obstacles) ->
     ok.
 
 move(AnimalName, {X, Y}) ->
-    %%% TODO -> implement the lookup in the grid and
-    %%% obstacle db to make sure that the move is valid.
-    gen_server:call(?MODULE, {move_coords, AnimalName, {X, Y}}).
+    % Check movement is in grid
+    [GridDimensions] = ets:lookup(gridDb, gridSize),
+    {_, Xmax, Ymax} = GridDimensions,
+    if
+        (X > Xmax) or (Y > Ymax) ->
+            throw("Outside of grid range. You'll fall off the earth");
+        true ->
+            ok
+    end,
+    % Check there are no obstacles
+    check_obstacles({X, Y}, palm),
+    check_obstacles({X, Y}, rock),
+    check_obstacles({X, Y}, water),
+    % Check there are no other animals...
+    check_animals(X, Y),
+
     %%% Think about new animals, and storing positions permanently
+    
+    io:format("~p: Moved to position {~p,~p}~n", [AnimalName, X, Y]),
+    gen_server:cast(?MODULE, {move_coords, AnimalName, {X, Y}}).
+
+check_animals(X, Y) ->
+    AnimalCheck = lists:flatten(ets:match(worldAnimalDb, #animal{name='$1', xPosition=X, yPosition=Y})),
+    check_animal_clash(AnimalCheck).
+
+check_animal_clash([]) ->
+    ok;
+check_animal_clash([H|T]) ->
+    throw("There is an animal in the area you're trying to move to").
+
+check_obstacles(Coords, Obstacle) ->
+    [ObstacleRow] = ets:lookup(obstacleDb, Obstacle),
+    {_, ObstacleCoords} = ObstacleRow,
+    check_coordinates_clash(Coords, ObstacleCoords).
+
+check_coordinates_clash(_AnimalCoords, []) -> ok;
+check_coordinates_clash(AnimalCoords, [H|T]) ->
+    if 
+        H == AnimalCoords ->
+            throw("There is an obstacle in the area you're trying to move to");
+        true ->
+            check_coordinates_clash(AnimalCoords, T)
+    end,
+    ok.
 
 get(AnimalName) ->
+    [AnimalLocation] = ets:lookup(worldAnimalDb, AnimalName),
+    {_,_,X,Y} = AnimalLocation,
+    io:format("~p is in location ~p,~p~n", [AnimalName, X, Y]),
     ok.
 
-delete(AnimalName) ->
-    ok.
+% delete(AnimalName) ->
+%     % gen_server:cast(?MODULE, {delete, AnimalName}).
+%     exit(whereis(AnimalName), kill).
 
-handle_cast() ->
-    ok.
-
-handle_call({move_coords, AnimalName, {X, Y}}, _From, AnimalDB) ->
-    {reply, animal:move_coords(AnimalName, {X, Y}), AnimalDB}.
+handle_cast({move_coords, AnimalName, {X, Y}}, AnimalDb) ->
+    {noreply, animal:move_coords(AnimalName, {X, Y}, AnimalDb)};
+handle_cast({delete, AnimalName}, AnimalDb) ->
+    {noreply, animal:delete(AnimalName, AnimalDb)}.
